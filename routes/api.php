30d85b8c3d50bba10ea0f6e06cc6157b3fb2c348
yatogahmisora <yatogahmisora@gmail.com>
2026-07-03 11:07:52 +0700

    Add Vercel

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,3 @@
-<?php
+<?php 
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+require __DIR__ . '/../public/index.php';
